87ec6bd203c818fb30c70aaf6566c35f2f399dc3
Shraban Chakma <shrabanchakma328@gmail.com>
2024-05-10 21:12:07 +0600

    added review slider with buttons

--- Client/Client/src/components/Rooms/RoomReview/RoomReview.jsx
+++ Client/Client/src/components/Rooms/RoomReview/RoomReview.jsx
@@ -1,36 +1,81 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import RoomReviewBox from "./RoomReviewBox";
+import ContainerTwo from "../../Shared/ContainerTwo";
+import { useState } from "react";
+import { CiCircleChevRight, CiCircleChevLeft } from "react-icons/ci";
 const RoomReview = () => {
+  const [swiperInstance, setSwiperInstance] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const handleMouseEnter = () => {
+    setIsVisible(true);
+  };
+  const handleMouseLeave = () => {
+    setIsVisible(false);
+  };
+  console.log(swiperInstance);
   return (
-    <div className="flex items-center h-44 my-20 w-5/6">
-      {/* average review / review stat / total review */}
-      <div className="h-full  flex flex-col justify-start bg-blue-500">
-        <h1 className="text-4xl font-medium">7/10</h1>
-        <h2 className="text-2xl font-medium">Good</h2>
-        <p className="text-sm">431 verified reviews </p>
+    <ContainerTwo>
+      <div className="flex items-center h-56 my-20 ">
+        {/* average review / review stat / total review */}
+        <div className="h-full w-1/4 flex flex-col justify-start p-4 bg-blue-500">
+          <h1 className="text-4xl font-medium">7/10</h1>
+          <h2 className="text-2xl font-medium">Good</h2>
+          <p className="text-sm">431 verified reviews </p>
+        </div>
+
+        <div className="w-2/3 h-full relative bg-slate-400">
+          <h1 className="font-medium text-gray-700">RecentReviews</h1>
+          <button
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className={` ${
+              isVisible ? "opacity-100" : "opacity-0    "
+            } absolute left-0 top-1/2 z-10 bg-white rounded-full transition-opacity ease-in duration-150`}
+            onClick={() => swiperInstance.slidePrev()}
+          >
+            <CiCircleChevLeft className="text-sky-700 text-4xl" />
+          </button>
+          <button
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className={` ${
+              isVisible ? "opacity-100" : "opacity-0    "
+            } absolute right-0 top-1/2 z-10 bg-white rounded-full transition-opacity ease-in duration-150`}
+            onClick={() => swiperInstance.slideNext()}
+          >
+            <CiCircleChevRight className="text-sky-700 text-4xl" />
+          </button>
+          <div
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="h-full w-full"
+          >
+            <Swiper
+              spaceBetween={0}
+              slidesPerView={2}
+              onSwiper={(swiper) => setSwiperInstance(swiper)}
+              className="h-full"
+            >
+              <SwiperSlide>
+                <RoomReviewBox />
+              </SwiperSlide>
+              <SwiperSlide>
+                <RoomReviewBox />
+              </SwiperSlide>
+              <SwiperSlide>
+                <RoomReviewBox />
+              </SwiperSlide>
+              <SwiperSlide>
+                <RoomReviewBox />
+              </SwiperSlide>
+              <SwiperSlide>
+                <RoomReviewBox />
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </div>
       </div>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={2}
-        className="h-full w-2/3 bg-green-500"
-      >
-        <SwiperSlide>
-          <RoomReviewBox />
-        </SwiperSlide>
-        <SwiperSlide>
-          <RoomReviewBox />
-        </SwiperSlide>
-        <SwiperSlide>
-          <RoomReviewBox />
-        </SwiperSlide>
-        <SwiperSlide>
-          <RoomReviewBox />
-        </SwiperSlide>
-        <SwiperSlide>
-          <RoomReviewBox />
-        </SwiperSlide>
-      </Swiper>
-    </div>
+    </ContainerTwo>
   );
 };
 
--- Client/Client/src/components/Rooms/RoomReview/RoomReviewBox.jsx
+++ Client/Client/src/components/Rooms/RoomReview/RoomReviewBox.jsx
@@ -1,11 +1,15 @@
 const RoomReviewBox = () => {
   // review box
   return (
-    <div className="h-full w-80 bg-white rounded-lg border-[1px] border-gray-400 space-y-4">
+    <div className="h-full w-[300px] bg-white rounded-lg border-[1px] border-gray-400 space-y-4">
       <div className="p-3">
         {/* review heading and description */}
         <div>
           <h1 className="font-medium ">8/10 Very good</h1>
+          <p className="text-[12px]">
+            The bed was not made, no sheets, coffee maker was half full and not
+            clean at all. You cannot get ahold of anyone after 11 pm
+          </p>
           <p className="text-blue-500 ">Read more</p>
         </div>
         <div>
