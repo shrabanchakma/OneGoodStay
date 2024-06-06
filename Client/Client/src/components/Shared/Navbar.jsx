db575fabdf198b3a7c6666596b2a6b1c423d98ce
Shraban Chakma <shrabanchakma328@gmail.com>
2024-06-07 01:42:37 +0600

    refactor: add daiysiui and update navbar component

--- Client/Client/src/components/Shared/Navbar.jsx
+++ Client/Client/src/components/Shared/Navbar.jsx
@@ -1,24 +1,72 @@
 import { Link } from "react-router-dom";
 import DropdownMenu from "./DropdownMenu";
 import Logo from "./Logo";
+import Container from "./Container";
 
 const Navbar = () => {
+  const navItems = (
+    <>
+      <li>
+        <Link to="/support" className="text-lg font-medium text-[#e41b43]">
+          Support
+        </Link>
+      </li>
+      <li>
+        <Link to="/about-us" className="text-lg font-medium text-[#e41b43]">
+          About us
+        </Link>
+      </li>
+      {/* dropdown menu */}
+      <DropdownMenu />
+    </>
+  );
   return (
-    <div className="w-full mx-auto bg-white border-b-[1px] border-gray-300 drop-shadow-xl mb-12">
-      <nav className="flex justify-between items-center h-24  max-w-[1280px] mx-auto">
-        <Logo />
-        <div className="flex items-center gap-3 ">
-          <Link to="/support" className="text-lg font-medium text-[#e41b43]">
-            Support
-          </Link>
-          <Link to="/about-us" className="text-lg font-medium text-[#e41b43]">
-            About us
-          </Link>
-          {/* dropdown menu */}
-          <DropdownMenu />
+    <>
+      <Container>
+        <div className="navbar h-32 bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              {/* small screen */}
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                {navItems}
+              </ul>
+            </div>
+            <a href="/" className="text-xl">
+              <Logo />
+            </a>
+          </div>
+          <div className="navbar-end hidden lg:flex">
+            {/* big screen */}
+            <div>
+              <ol className="flex items-center gap-3">{navItems}</ol>
+            </div>
+          </div>
         </div>
-      </nav>
-    </div>
+      </Container>
+    </>
   );
 };
 
